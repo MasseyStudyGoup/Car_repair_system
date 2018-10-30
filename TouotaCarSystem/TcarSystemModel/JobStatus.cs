dab66ea12a568111942eb5470962158497050ebf
RebeccaLiu0 <liunanno2@gmail.com>
2018-10-30 15:34:53 +1300

    job

--- TouotaCarSystem/TcarSystemModel/JobStatus.cs
+++ TouotaCarSystem/TcarSystemModel/JobStatus.cs
@@ -5,5 +5,7 @@ using System.Text;
 namespace TcarSystem.Model
 {
     public enum JobStatus { Unconfirmed, Open, Assigned, Closed }
-    
+    //Desk can confirm/update/close a job. if confirm, the job's status is changed to open
+    //if close, the job's status is changed to close, resolve status needs to be changed.
+
 }
