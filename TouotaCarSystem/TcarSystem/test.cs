aa37ceec06f8a311ca4e4476d112f1f6e712eb66
RebeccaLiu0 <liunanno2@gmail.com>
2018-10-30 18:10:19 +1300

    R

---
+++ TouotaCarSystem/TcarSystem/test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TcarSystem.Model;
+using TcarSystem.BLL;
+
+namespace TcarSystem
+{
+    public partial class test : Form
+    {
+        JobInfo job;
+
+
+        public test()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            job.
+        }
+    }
+}
