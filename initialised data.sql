bcdc4dda970a3c8878927b9db3d00832b8991b31
RebeccaLiu0 <liunanno2@gmail.com>
2018-11-06 14:41:16 +1300

    data

--- initialised data.sql
+++ initialised data.sql
@@ -35,6 +35,7 @@ CREATE TABLE jobs (
 	jobStatus INTEGER,
 	opendate TEXT DEFAULT '',
 	assigndate TEXT DEFAULT '',
+	jobType TEXT DEFAULT '',
 	FOREIGN KEY(customer) REFERENCES user(userId),
 	FOREIGN KEY(worker) REFERENCES user(userId),
 	FOREIGN KEY(manager) REFERENCES user(userId),
@@ -58,7 +59,13 @@ INSERT INTO user(user_name,carNo,user_password,identity,Outlet) VALUES ('R','HTZ
 INSERT INTO user(user_name,carNo,user_password,identity,Outlet) VALUES ('Rtest','HTZ123','r123','user',);
 INSERT INTO user(user_name,carNo,user_password,identity,Outlet) VALUES ('user1','U111','r123','user',NULL);
 INSERT INTO user(user_name,carNo,user_password,identity,Outlet) VALUES ('user2','U111','r123','user',NULL);
-
+INSERT INTO user(user_name,carNo,user_password,identity,Outlet) VALUES ('user3','U333','r123','user',NULL);
+INSERT INTO user(user_name,carNo,user_password,identity,Outlet) VALUES ('user3','U444','r123','user',NULL);
+INSERT INTO user(user_name,carNo,user_password,identity,Outlet) VALUES ('user4','U444','r123','user',NULL);
+INSERT INTO user(user_name,carNo,user_password,identity,Outlet) VALUES ('user5','U555','r123','user',NULL);
+INSERT INTO user(user_name,carNo,user_password,identity,Outlet) VALUES ('Rde',' ','r123','hpdesk',2);
+INSERT INTO user(user_name,carNo,user_password,identity,Outlet) VALUES ('Rma',' ','r123','manager',2);
+INSERT INTO user(user_name,carNo,user_password,identity,Outlet) VALUES ('Rwo',' ','r123','work',2);
 
 --add jobs;
 INSERT INTO jobs ( closedate, createdate, carNo, desk, manager, worker, jobDescription, resolve, priority, comment, outlet,customer,jobStatus,opendate,assigndate)
@@ -72,6 +79,33 @@ VALUES('','2018-11-06','HTZ123',NULL,NULL,NULL,'Rear door broke',NULL,1,NULL,2,9
 
 INSERT INTO jobs ( closedate, createdate, carNo, desk, manager, worker, jobDescription, resolve, priority, comment, outlet,customer,jobStatus,opendate,assigndate)
 VALUES('','2018-11-06','U111',NULL,NULL,NULL,'Rear door broke',NULL,1,NULL,2,10,0,NULL,NULL);
+
+INSERT INTO jobs ( closedate, createdate, carNo, desk, manager, worker, jobDescription, resolve, priority, comment, outlet,customer,jobStatus,opendate,assigndate)
+VALUES('','2018-11-06','U111',NULL,NULL,NULL,'Rear door broke',NULL,1,NULL,2,11,0,NULL,NULL);
+
+INSERT INTO jobs ( closedate, createdate, carNo, desk, manager, worker, jobDescription, resolve, priority, comment, outlet,customer,jobStatus,opendate,assigndate)
+VALUES('','2018-11-06','U111',12,13,NULL,'Rear door broke',NULL,1,NULL,2,11,1,'2018-11-06',NULL);
+
+INSERT INTO jobs ( closedate, createdate, carNo, desk, manager, worker, jobDescription, resolve, priority, comment, outlet,customer,jobStatus,opendate,assigndate)
+VALUES('','2018-11-06','U111',12,13,NULL,'window broke',NULL,1,NULL,2,11,1,'2018-11-06',NULL);
+
+INSERT INTO jobs ( closedate, createdate, carNo, desk, manager, worker, jobDescription, resolve, priority, comment, outlet,customer,jobStatus,opendate,assigndate)
+VALUES('','2018-11-06','U444',12,13,NULL,'back window broke',NULL,1,NULL,2,15,1,'2018-11-06',NULL);
+
+INSERT INTO jobs ( closedate, createdate, carNo, desk, manager, worker, jobDescription, resolve, priority, comment, outlet,customer,jobStatus,opendate,assigndate)
+VALUES('','2018-11-06','U555',12,13,NULL,'back window broke',NULL,1,NULL,2,16,1,'2018-11-06',NULL);
+
+INSERT INTO jobs ( closedate, createdate, carNo, desk, manager, worker, jobDescription, resolve, priority, comment, outlet,customer,jobStatus,opendate,assigndate)
+VALUES('','2018-11-06','U666',12,13,14,'back seat broke',NULL,1,NULL,2,17,1,'2018-11-06','2018-11-06');
+
+INSERT INTO jobs ( closedate, createdate, carNo, desk, manager, worker, jobDescription, resolve, priority, comment, outlet,customer,jobStatus,opendate,assigndate)
+VALUES('','2018-11-06','U667',12,13,14,'back seat broke',NULL,1,NULL,2,18,1,'2018-11-06','2018-11-06');
+
+INSERT INTO jobs ( closedate, createdate, carNo, desk, manager, worker, jobDescription, resolve, priority, comment, outlet,customer,jobStatus,opendate,assigndate)
+VALUES('','2018-11-06','U668',12,13,14,'back seat broke',NULL,1,NULL,2,18,1,'2018-11-06','2018-11-06');
+
+INSERT INTO jobs ( closedate, createdate, carNo, desk, manager, worker, jobDescription, resolve, priority, comment, outlet,customer,jobStatus,opendate,assigndate)
+VALUES('','2018-11-06','U669',12,13,14,'back seat broke',NULL,1,NULL,2,16,1,'2018-11-06','2018-11-06');
 
 
       
