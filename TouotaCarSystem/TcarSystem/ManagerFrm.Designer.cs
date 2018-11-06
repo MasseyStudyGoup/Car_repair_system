153c0b0ff23c8ae97a4c2588d9cfad73b0a8bc0a
portfolio <wangziyizoe@gmail.com>
2018-11-06 19:43:15 +1300

    update file

--- TouotaCarSystem/TcarSystem/ManagerFrm.Designer.cs
+++ TouotaCarSystem/TcarSystem/ManagerFrm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ManagerFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1082, 592);
+            this.Name = "ManagerFrm";
             this.Text = "ManagerFrm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
