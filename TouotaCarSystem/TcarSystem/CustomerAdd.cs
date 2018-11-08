ea6853630f55c7cefba32bf802ce6ec1e9861288
RebeccaLiu0 <liunanno2@gmail.com>
2018-11-08 13:11:05 +1300

    update

---
+++ TouotaCarSystem/TcarSystem/CustomerAdd.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TcarSystem.Model;
+using TcarSystem.BLL;
+
+namespace TcarSystem
+{
+    public partial class CustomerAdd : Form
+    {
+        private JobInfo m_job;
+        private static UserInfor user = UserInforBLL.CurrentUser;
+        public CustomerAdd()
+        {
+            m_job = new JobInfo();
+            InitializeComponent();
+            this.CancelButton = btn_cancel;
+            this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+
+        public CustomerAdd(JobInfo currentJob)
+        {
+            m_job = currentJob;
+            InitializeComponent();
+            this.CancelButton = btn_cancel;
+            this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+
+        }
+
+        private void CustomerAdd_Load(object sender, EventArgs e)
+        {
+            cbType.DataSource = System.Enum.GetNames(typeof(JobType));
+            foreach (Outlet o in JobInfoManager.getAllOutletsBycus())
+            {
+                ItemData item = new ItemData(o.Name, o);
+                cbOutlet.Items.Add(item);
+                if (m_job.outlet != null && o.Id == m_job.outlet.Id)
+                    cbOutlet.SelectedItem = item;
+            }
+            cbType.DataSource = System.Enum.GetNames(typeof(JobType));
+            if (m_job.id != null)
+            {
+
+                txCarNo.Text = m_job.carNo;
+               // cbOutlet.Text = (m_job.outlet == null) ? "" : m_job.outlet.Name;
+                cbType.SelectedText = m_job.jobType.ToString();
+                txJobDes.Text = m_job.jobDescription;
+
+
+            }
+        }
+
+        private void cbOutlet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            cbType.DataSource = System.Enum.GetNames(typeof(JobType));
+            foreach (Outlet o in JobInfoManager.getAllOutletsBycus())
+            {
+                ItemData item = new ItemData(o.Name, o);
+                cbOutlet.Items.Add(item);
+                if (m_job.outlet != null && o.Id == m_job.outlet.Id)
+                    cbOutlet.SelectedItem = item;
+            }
+
+
+            m_job.carNo = txCarNo.Text;
+            m_job.jobType = (JobType)Enum.Parse(typeof(JobType), cbType.Text);
+            m_job.jobDescription = txJobDes.Text;
+            m_job.priority = Priority.Normal;
+            m_job.createdate = DateTime.Now;
+            m_job.customer = user;
+            m_job.outlet = (Outlet)((ItemData)cbOutlet.SelectedItem).Value;
+            m_job.jobStatus = JobStatus.Unconfirmed;
+
+
+            if (m_job.id == null)
+                JobInfoManager.AddJob(m_job);
+            else if(m_job.jobStatus == JobStatus.Unconfirmed)
+                JobInfoManager.UpdateJob(m_job);
+            else
+                MessageBox.Show("Sorry, cannot modify the job!");
+            DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+    }
+}
---
+++ TouotaCarSystem/TcarSystem/customerForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TcarSystem.Model;
+using TcarSystem.BLL;
+
+namespace TcarSystem
+{
+    public partial class customerForm : Form
+    {
+        private Dictionary<string, JobInfo> m_jobDict = new Dictionary<string, JobInfo>();
+        public customerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void customerForm_Load(object sender, EventArgs e)
+        {
+            cusJobList.Rows.Clear();
+            m_jobDict.Clear();
+            IList<JobInfo> jobs = JobInfoManager.GetAllJobInfos();
+
+            if (jobs != null)
+            {
+                foreach (JobInfo job in jobs)
+                {
+
+                    m_jobDict.Add("" + job.id, job);
+                    cusJobList.Rows.Add(new string[] {
+                    job.id.ToString(),
+                    job.carNo,
+                    (job.customer==null)?"":job.customer.UserName,
+                    (job.outlet==null)?"":job.outlet.Name,
+                    job.jobType.ToString(),
+                    //job.priority.ToString(),
+                    job.jobStatus.ToString(),
+                    job.jobDescription,
+                    (job.desk==null)?"":job.desk.UserName,
+                    job.resolve.ToString(),
+                    job.comment,
+                    job.createdate.ToString("yyyy-MM-dd HH:mm:ss")
+
+                });
+                }
+            }
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            CustomerAdd jobForm = new CustomerAdd();
+            jobForm.ShowDialog();
+            customerForm_Load(null, null);
+        }
+
+        private void btnUpdataJob_Click(object sender, EventArgs e)
+        {
+            EditJob();
+        }
+
+        
+
+        private void cusJobList_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            EditJob();
+        }
+
+        private void EditJob()
+        {
+            JobInfo job = m_jobDict[cusJobList.SelectedRows[0].Cells[0].Value.ToString()];
+            if (job != null)
+            {
+                CustomerAdd jobForm = new CustomerAdd(job);
+                jobForm.ShowDialog();
+                customerForm_Load(null, null);
+
+            }
+        }
+    }
+}
